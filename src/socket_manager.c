06e0eda17e13a94993c7f2758419e2bcbdd64801
唐成 <tangcheng2005@gmail.com>
2014-04-20 22:50:17 +0800

    add socket_manager class

---
+++ src/socket_manager.c
@@ -0,0 +1,155 @@
+#include "socket_manager.h"
+#include <zmq.h>
+#include <assert.h>
+#include <stdlib.h>
+
+typedef struct _IpcamSocketManagerHashValue
+{
+    void *mq_socket;
+    gint type;
+} IpcamSocketManagerHashValue;
+
+typedef struct _IpcamSocketManagerPrivate
+{
+    GHashTable *socket_hash;
+} IpcamSocketManagerPrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE(IpcamSocketManager, ipcam_socket_manager, G_TYPE_OBJECT);
+
+static GObject *ipcam_socket_manager_constructor(GType self_type,
+                                                 guint n_properties,
+                                                 GObjectConstructParam *properties)
+{
+    GObject *obj;
+    obj = G_OBJECT_CLASS(ipcam_socket_manager_parent_class)->constructor(self_type, n_properties, properties);
+    return obj;
+}
+static void ipcam_socket_manager_dispose(GObject *self)
+{
+    static gboolean first_run = TRUE;
+
+    if (first_run)
+    {
+        first_run = FALSE;
+        G_OBJECT_CLASS(ipcam_socket_manager_parent_class)->dispose(self);
+        IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(IPCAM_SOCKET_MANAGER(self));
+        g_hash_table_destroy(priv->socket_hash);
+    }
+}
+static void ipcam_socket_manager_finalize(GObject *self)
+{
+    G_OBJECT_CLASS(ipcam_socket_manager_parent_class)->finalize(self);
+}
+static void destroy_notify(gpointer data)
+{
+    IpcamSocketManagerHashValue *value = (IpcamSocketManagerHashValue *)data;
+    free(value);
+}
+static void ipcam_socket_manager_init(IpcamSocketManager *self)
+{
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(self);
+    priv->socket_hash = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, (GDestroyNotify)destroy_notify);
+    assert(priv->socket_hash);
+}
+static void ipcam_socket_manager_class_init(IpcamSocketManagerClass *klass)
+{
+    GObjectClass *this_class = G_OBJECT_CLASS(klass);
+    this_class->constructor = &ipcam_socket_manager_constructor;
+    this_class->dispose = &ipcam_socket_manager_dispose;
+    this_class->finalize = &ipcam_socket_manager_finalize;
+}
+gboolean ipcam_socket_manager_add(IpcamSocketManager *socket_manager,
+                                  const gchar *name,
+                                  const int type,
+                                  const void *mq_socket)
+{
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(socket_manager);
+    IpcamSocketManagerHashValue *value = (IpcamSocketManagerHashValue *)malloc(sizeof(IpcamSocketManagerHashValue));
+    g_return_val_if_fail(value, FALSE);
+    value->mq_socket = (void *)mq_socket;
+    value->type = type;
+    g_hash_table_insert(priv->socket_hash, (gpointer)name, (gpointer)value);
+    return TRUE;
+}
+static gboolean finder(gpointer key, gpointer value, gpointer user_data)
+{
+    gboolean ret = FALSE;
+    IpcamSocketManagerHashValue *hash_value = (IpcamSocketManagerHashValue *)value;
+    if (hash_value->mq_socket == (void *)user_data)
+    {
+        ret = TRUE;
+    }
+    return ret;
+}
+gboolean ipcam_socket_manager_delete_by_socket(IpcamSocketManager *socket_manager, const void *mq_socket)
+{
+    g_return_val_if_fail(IPCAM_IS_SOCKET_MANAGER(socket_manager), FALSE);
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(socket_manager);
+    return 0 != g_hash_table_foreach_remove(priv->socket_hash, (GHRFunc)finder, (gpointer)mq_socket);
+}
+gboolean ipcam_socket_manager_delete_by_name(IpcamSocketManager *socket_manager, const gchar *name)
+{
+    g_return_val_if_fail(IPCAM_IS_SOCKET_MANAGER(socket_manager), FALSE);
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(socket_manager);
+    return g_hash_table_remove(priv->socket_hash, name);
+}
+gboolean ipcam_socket_manager_has_name(IpcamSocketManager *socket_manager, const gchar *name)
+{
+    g_return_val_if_fail(IPCAM_IS_SOCKET_MANAGER(socket_manager), FALSE);
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(socket_manager);
+    return g_hash_table_contains(priv->socket_hash, name);
+}
+gboolean ipcam_socket_manager_has_socket(IpcamSocketManager *socket_manager, const void *mq_socket)
+{
+    g_return_val_if_fail(IPCAM_IS_SOCKET_MANAGER(socket_manager), FALSE);
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(socket_manager);
+    return NULL != g_hash_table_find(priv->socket_hash, (GHRFunc)finder, (gpointer)mq_socket);   
+}
+gboolean ipcam_socket_manager_get_by_name(IpcamSocketManager *socket_manager,
+                                          const gchar *name,
+                                          int *type,
+                                          void **mq_socket)
+{
+    g_return_val_if_fail(IPCAM_IS_SOCKET_MANAGER(socket_manager), FALSE);
+    gboolean ret = FALSE;
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(socket_manager);
+    IpcamSocketManagerHashValue *value =
+        (IpcamSocketManagerHashValue *)g_hash_table_lookup(priv->socket_hash, name);
+    if (NULL != value)
+    {
+        *type = value->type;
+        *mq_socket = value->mq_socket;
+        ret = TRUE;
+    }
+    return ret;
+}
+gboolean ipcam_socket_manager_get_by_socket(IpcamSocketManager *socket_manager,
+                                            const void *mq_socket,
+                                            gchar *name,
+                                            int *type)
+{
+    g_return_val_if_fail(IPCAM_IS_SOCKET_MANAGER(socket_manager), FALSE);
+    gboolean ret = FALSE;
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(socket_manager);
+    IpcamSocketManagerHashValue *value =
+        g_hash_table_find(priv->socket_hash, (GHRFunc)finder, (gpointer)mq_socket);
+    if (NULL != value)
+    {
+        *type = value->type;
+        // ToDo: find the name
+        ret = TRUE;
+    }
+    return ret;
+}
+static void iterator(gpointer key, gpointer value, gpointer user_data)
+{
+    IpcamSocketManagerHashValue *item = (IpcamSocketManagerHashValue *)value;
+    zmq_close(item->mq_socket);
+}
+void ipcam_socket_manager_close_all_socket(IpcamSocketManager *socket_manager)
+{
+    g_return_if_fail(IPCAM_IS_SOCKET_MANAGER(socket_manager));
+    IpcamSocketManagerPrivate *priv = ipcam_socket_manager_get_instance_private(socket_manager);
+    g_hash_table_foreach(priv->socket_hash, (GHFunc)iterator, NULL);
+    g_hash_table_remove_all(priv->socket_hash);
+}
---
+++ src/socket_manager.h
@@ -0,0 +1,37 @@
+#ifndef __SOCKET_MANAGER_H__
+#define __SOCKET_MANAGER_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#define IPCAM_SOCKET_MANAGER_TYPE (ipcam_socket_manager_get_type())
+#define IPCAM_SOCKET_MANAGER(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), IPCAM_SOCKET_MANAGER_TYPE, IpcamSocketManager))
+#define IPCAM_SOCKET_MANAGER_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), IPCAM_SOCKET_MANAGER_TYPE, IpcamSocketManagerClass))
+#define IPCAM_IS_SOCKET_MANAGER(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), IPCAM_SOCKET_MANAGER_TYPE))
+#define IPCAM_IS_SOCKET_MANAGER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), IPCAM_SOCKET_MANAGER_TYPE))
+#define IPCAM_SOCKET_MANAGER_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), IPCAM_SOCKET_MANAGER_TYPE, IpcamSocketManagerClass))
+
+typedef struct _IpcamSocketManager IpcamSocketManager;
+typedef struct _IpcamSocketManagerClass IpcamSocketManagerClass;
+
+struct _IpcamSocketManager
+{
+    GObject parent;
+};
+
+struct _IpcamSocketManagerClass
+{
+    GObjectClass parent_class;
+};
+
+GType ipcam_socket_manager_get_type(void);
+gboolean ipcam_socket_manager_add(IpcamSocketManager *socket_manager, const gchar *name, const int type, const void *mq_socket);
+gboolean ipcam_socket_manager_delete_by_socket(IpcamSocketManager *socket_manager, const void *mq_socket);
+gboolean ipcam_socket_manager_delete_by_name(IpcamSocketManager *socket_manager, const gchar *name);
+gboolean ipcam_socket_manager_has_name(IpcamSocketManager *socket_manager, const gchar *name);
+gboolean ipcam_socket_manager_has_socket(IpcamSocketManager *socket_manager, const void *mq_socket);
+gboolean ipcam_socket_manager_get_by_name(IpcamSocketManager *socket_manager, const gchar *name, int *type, void **mq_socket);
+gboolean ipcam_socket_manager_get_by_socket(IpcamSocketManager *socket_manager, const void *mq_socket, gchar *name, int *type);
+void ipcam_socket_manager_close_all_socket(IpcamSocketManager *socket_manager);
+
+#endif /* __SOCKET_MANAGER_H__ */
