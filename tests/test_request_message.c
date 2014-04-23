52c804700b2cd2cefa00053fd4dc575083ef6fe0
唐成 <tangcheng2005@gmail.com>
2014-04-23 16:05:22 +0800

    测试代码中加入释放内存的语句g_object_unref

--- tests/test_request_message.c
+++ tests/test_request_message.c
@@ -3,8 +3,12 @@
 int main(int argc, char* argv[])
 {
     IpcamRequestMessage *request_message = g_object_new(IPCAM_REQUEST_MESSAGE_TYPE, NULL);
+    g_object_set(G_OBJECT(request_message), "message-action", "test", NULL);
     IpcamMessage *message = ipcam_request_message_get_response_message(request_message, "-1");
+    g_object_unref(request_message);
+    g_object_unref(message);
 
     request_message = g_object_new(IPCAM_REQUEST_MESSAGE_TYPE, NULL);
+    g_object_unref(request_message);
     return 0;
 }
