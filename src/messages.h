54e8f93d7a00cb9ded676deb380eb3892a1b6c6c
唐成 <tangcheng2005@gmail.com>
2014-05-03 15:33:26 +0800

    增加messages.h头文件

---
+++ src/messages.h
@@ -0,0 +1,9 @@
+#ifndef __MESSAGES_H__
+#define __MESSAGES_H__
+
+#include <message.h>
+#include <notice_message.h>
+#include <request_message.h>
+#include <response_message.h>
+
+#endif /* __MESSAGES_H__ */
